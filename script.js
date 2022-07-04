4e9eae4ef5dd5cc52f2e1df7b5fdd70709f9b298
NorbertCyman <78094832+NorbertCyman@users.noreply.github.com>
2022-07-04 12:29:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const nav = document.querySelector(".nav");
 let lastScrollY = window.scrollY;
 let out = false;
-console.log(nav);
 console.log(window.innerWidth);
 window.addEventListener("scroll", () => {
     if (window.innerWidth > 500){
@@ -13,21 +12,16 @@ window.addEventListener("scroll", () => {
         lastScrollY = window.scrollY;
     }
 });
-window.addEventListener("resize", () => {
-    console.log(window.innerWidth);
-    if(window.innerWidth >= 500){
-        nav.classList.add("nav-hidden");
-    }
-});
+if(window.innerWidth >= 500){
+    nav.classList.add("nav-hidden");
+}
 
 function openMenu(){
     if(out){
         nav.classList.add("nav-hidden");
         out = false;
-        console.log("1");
     }
     else{nav.classList.remove("nav-hidden");
     out = true;
-    console.log(2);
     }
 }
